461972488522fa93c607d28875692ca5cde92dd1
CruiseQuan <568336119@qq.com>
2015-09-17 20:21:03 +0800

    Create No_26RemoveDuplicatesfromSortedArray.java

---
+++ No_26RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeDuplicates(int[] A) {
+         int n=A.length;
+          if(n<2)
+              return n;
+          int index=1;
+          for(int i=1;i<n;i++)
+              if(A[i]!=A[i-1])
+                  A[index++]=A[i];
+          return index;
+    }
+}
